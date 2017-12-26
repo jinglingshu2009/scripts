18b47bc3c43de6aa1ee611d88ef99a3d21a68dc0
jinglingshu2009 <jinglingshu2009@163.com>
2017-12-26 21:40:27 +0800

    增加bat对应脚本

---
+++ bat/create_table.bat
@@ -0,0 +1,16 @@
+
+CREATE TABLE  send2 (
+id  int,
+name char(255) ,
+AGE    number  ,
+address1 varchar(500),
+address2 varchar(400),
+address3 varchar(400),
+address4 varchar(400),
+address5 varchar(400),
+address6 varchar(400),
+address7 varchar(400),
+address8 varchar(400),
+address9 varchar(400),
+address10 varchar(400)
+)
