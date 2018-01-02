0c3467c14fa1084a2639d7048e67022ab9ac4c6f
jinglingshu2009 <jinglingshu2009@163.com>
2018-01-02 23:50:33 +0800

    更新setup.py study_python hello.py  学习python脚本的cx_freeze方式的打包

---
+++ python/.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#^_^ coding:utf-8 ^_^
+# Filename:.py
+#!/usr/bin/python
+#^_^ coding:utf-8 ^_^
+# Filename:.py
+#!/usr/bin/python
+#^_^ coding:utf-8 ^_^
+# Filename:.py
--- python/hello.py
+++ python/hello.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 #^_^ coding:utf-8 ^_^
 #Filename:hello.py
+import time
 def print_welcome():
     u'''
     自定义函数
     自定义函数：示例print函数.'''
 print u'Hello,python!!!'
-
+time.sleep(3)
 print_welcome()
 print print_welcome.__doc__
+time.sleep(3)
