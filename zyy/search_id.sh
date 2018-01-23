fc4a98e361c77492f1b484abed4cfc703723b813
jinglingshu2009 <jinglingshu2009@163.com>
2018-01-23 23:03:07 +0800

    更新 search_id.sh  添加部分注释

--- zyy/search_id.sh
+++ zyy/search_id.sh
@@ -5,13 +5,13 @@ IFS=$(echo -en "\n\b")
 echo -en $IFS
 ##########################
 
-_wc=`wc -l tp_spec_goods_price.csv|awk '{print $1}'`
+_wc=`wc -l tp_spec_goods_price.csv|awk '{print $1}'` #获取源文件行数
 id=0
 while [ $id -lt $_wc ]
 do
 	id=$(($id+1))
-	zz=`sed -n  "$id"p tp_spec_goods_price.csv`
-	get_spec=`echo $zz |awk -F, '{print $4}'`
+	zz=`sed -n  "$id"p tp_spec_goods_price.csv` #逐行获取源文件中的数据
+	get_spec=`echo $zz |awk -F, '{print $4}'`   #获取以逗号分隔的第四列数据
 	get_key_name=`echo $zz |awk -F, '{print $5}'`
 	get_goods_id=`echo $zz |awk -F, '{print $2}'`
 	if [ $id > 1 ];then
@@ -25,6 +25,5 @@ do
 	else
 		get_goods_id=`echo $zz |awk -F, '{print $2}'`
 		#echo $id	$get_goods_id	$get_spec	$get_key_name	$get_key>>get_key.txt
-
 	fi
 done
