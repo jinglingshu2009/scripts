c9241b23ad97af12e1b33ee89e5d824acab96b5e
jinglingshu2009 <jinglingshu2009@163.com>
2018-04-25 19:08:38 +0800

    增加Test自动生成文件脚本

--- python/Test_list.py
+++ python/Test_list.py
@@ -1,45 +1,8 @@
 #!/usr/bin/python
 #^_^ coding:utf-8 ^_^
 # Filename:Test_list.py
-def say_cut(src_data, cut, dst_data):
-    src_data = src_data + cut
-    # print src_data,cut,dst_data
-    wc = 0
-    dst_data = []
-    # tmp_data = tmp_data
-    for tmp_srcdata in src_data:
-        if tmp_srcdata != cut:
-            wc = wc + 1
-            if wc == 1:
-                tmp_data1 = tmp_srcdata
-            elif wc == 2:
-                tmp_data2 = tmp_srcdata
-            elif wc == 3:
-                tmp_data3 = tmp_srcdata
-            elif wc == 4:
-                tmp_data4 = tmp_srcdata
-            elif wc == 5:
-                tmp_data5 = tmp_srcdata
-        else:
-            if wc == 1:
-                info = tmp_data1
-            elif wc == 2:
-                info = tmp_data1 + tmp_data2
-            elif wc == 3:
-                info = tmp_data1 + tmp_data2 + tmp_data3
-            elif wc == 4:
-                info = tmp_data1 + tmp_data2 + tmp_data3 + tmp_data4
-            elif wc == 5:
-                info = tmp_data1 + tmp_data2 + tmp_data3 + tmp_data4 + tmp_data5
-            dst_data.append(info)
-            wc = 0
-    #print dst_data
-
-src_data = "1:22:333:4444:55555:fasdf:43:fasdf:fasf:42423:4@#$#:44234:666f76"
-cut = ":"
-dst_data = "dstdata"
-
-# print tmp_name
-#say_cut(src_data, cut, dst_data)
-print(len(src_data.split(':')))
-print src_data.split(':')
+file_name=""
+file_type  = "exe,doc,rar,docx,xls,txt,bin,tar,gif,bmp,img"
+type_list=file_type.split(",")
+for file_type in range(len(type_list)):
+    print "%s %s%s.%s " % (file_type,file_name,file_type,type_list[file_type])
